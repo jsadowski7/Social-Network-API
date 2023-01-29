42dfeffa9941474aa724a6276b898a2972376220
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-01-29 17:54:38 -0600

    Wrote routes for thoughtRoutes.js file

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,17 @@ const {
     createReaction,
     deleteReaction
 } = require('../../controllers/thoughtController');
+
+// Route for /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// Route for /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).post(updateThought).delete(deleteThought);
+
+// Route for /api/thoughts/:thoughtId/reaction
+router.route('/:thoughtId/reaction').post(createReaction).delete(deleteReaction);
+
+// Route for /api/thoughts/:thoughtId/reaction/:reactionId
+router.route('/:thoughtId/reaction/:reactionId').delete(deleteReaction);
+
+module.exports = router;

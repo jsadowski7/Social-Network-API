6ad3dbec47ea88352c25054c3d7f04b20bad9894
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-01-29 17:49:49 -0600

    Wrote routes for userRoutes.js file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,3 +8,14 @@ const {
     addFriend,
     deleteFriend,
 } = require('../../controllers/userController');
+
+// Route for /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// Route for /api/users/:userId
+router.route('/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
+
+// Route for /api/users/:userId/friends/:friendId
+router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;

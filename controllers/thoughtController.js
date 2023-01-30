0d2cec6b546edb923d8a19e285438577a317e6a1
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-01-29 18:00:55 -0600

    Layed out bones for controller functions to be exported

---
+++ controllers/thoughtController.js
@@ -0,0 +1,11 @@
+const { Thought } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {},
+    createThought(req, res) {},
+    getSingleThought(req, res) {},
+    updateThought(req, res) {},
+    deleteThought(req, res) {},
+    addReaction(req, res) {},
+    deleteReaction(req, res) {},
+}
---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const { User } = require('../models');
+
+module.exports = {
+    getUsers(req, res) {},
+    createUser(req, res) {},
+    getSingleUser(req, res) {},
+    updateUser(req, res) {},
+    deleteUser(req, res) {},
+    addFriend(req, res) {},
+    deleteFriend(req, res) {},
+}

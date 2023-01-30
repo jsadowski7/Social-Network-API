aa287522a810fe62b04cba9865918c3b0d6909e5
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-01-29 19:14:41 -0600

    Wrote user functions/routes for userController.js file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,72 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 module.exports = {
-    getUsers(req, res) {},
-    createUser(req, res) {},
-    getSingleUser(req, res) {},
-    updateUser(req, res) {},
-    deleteUser(req, res) {},
-    addFriend(req, res) {},
-    deleteFriend(req, res) {},
-}
+  getUsers(req, res) {
+    User.find()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $set: req.body })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.id })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User found" })
+          : res.json({ message: "User has been deleted" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};

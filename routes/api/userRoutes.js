794ad1a75722f46967c55596cbf095fdccafab34
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-01-31 15:48:34 -0600

    Fixed path for route in userRoutes.js, fixed DB connection in connection.js file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,6 +16,6 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
 
 // Route for /api/users/:userId/friends/:friendId
-router.route('/api/users/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
